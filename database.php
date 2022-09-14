476cf408189c1789b0311e0dd9c535703734df35
sairaka <104164647+sairaka@users.noreply.github.com>
2022-09-15 00:39:20 +0300

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 $host        = "host = localhost";
    $port        = "port = 5432";
-   $dbname      = "dbname = raisakfi_employee";
-   $credentials = "user = raisakfi_rkemployee password=JotainUutta22";
+   $dbname      = "dbname = database";
+   $credentials = "user = username password=userpassword";
    $db = pg_connect( "$host $port $dbname $credentials")
        or die("Could not connect");
     echo "Connected successfully";
