8864ddeb8f285f59a1e56d32c74f92cdc4588a95
sairaka <104164647+sairaka@users.noreply.github.com>
2022-10-11 10:18:47 +0300

    Delete database.php

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host        = "host = localhost";
-   $port        = "port = 5432";
-   $dbname      = "dbname = database";
-   $credentials = "user = username password=userpassword";
-   $db = pg_connect( "$host $port $dbname $credentials")
-       or die("Could not connect");
-    echo "Connected successfully";
-?>
